107eaf304690eed1a9b2bf11eab2183824421f6e
engswee <engswee@users.noreply.github.com>
2020-02-25 13:39:04 +0100

    XML2JSONConverter generator switched to native JsonOutput
    - Switch JSON parser in ConversionJSONOutput to groovy.json.JsonOutput
    --- JSONObject changed to Map
    --- JSONArray changed to List
    --- Indent set to 4 spaces (default and not customisable)
    
    - Updated test results
    --- change indent settings
    --- Add null field
    --- Change umlaut output to escaped sequence
    
    - Remove org.json classes and update openClover exclude in POM

--- src/main/groovy/com/equalize/converter/core/util/ConversionJSONOutput.groovy
+++ src/main/groovy/com/equalize/converter/core/util/ConversionJSONOutput.groovy
@@ -1,95 +1,105 @@
 package com.equalize.converter.core.util
 
-import org.json.JSONArray
-import org.json.JSONObject
+import groovy.json.JsonOutput
 
 class ConversionJSONOutput {
 
-	boolean forceArray = false
-	Set<String> arrayFields
-	Map<String, String> fieldConversions
+  boolean forceArray = false
+  Set<String> arrayFields
+  Map<String, String> fieldConversions
 
-	String generateJSONText(XMLElementContainer xmlElement, boolean skipRoot, int indentFactor) {
-		JSONObject jo = new JSONObject()
-		constructJSONContentfromXML(jo, xmlElement)
-		if (skipRoot) {
-			return getJSONText(jo, indentFactor)
-		} else {
-			JSONObject rootjo = new JSONObject()
-			rootjo.put(xmlElement.getElementName(), jo)
-			return getJSONText(rootjo, indentFactor)
-		}
-	}
+  String generateJSONText(XMLElementContainer xmlElement, boolean skipRoot, int indentFactor) {
+    def jsonObject = new LinkedHashMap()
+    constructJSONContentfromXML(jsonObject, xmlElement)
+    if (skipRoot) {
+      return getJSONText(jsonObject, indentFactor)
+    } else {
+      Map rootJsonObject = new LinkedHashMap()
+      rootJsonObject.put(xmlElement.getElementName(), jsonObject)
+      return getJSONText(rootJsonObject, indentFactor)
+    }
+  }
 
-	String generateJSONText(XMLElementContainer element, boolean skipRoot) {
-		return generateJSONText(element, skipRoot, 0)
-	}
+  String generateJSONText(XMLElementContainer element, boolean skipRoot) {
+    return generateJSONText(element, skipRoot, 0)
+  }
 
-	private void constructJSONContentfromXML(JSONObject parent, XMLElementContainer element) {
-		Map<String, JSONArray> map = new LinkedHashMap<String, JSONArray>()
-		// Process all the child fields of the XML element
-		for (Field childField : element.getChildFields()) {
-			// Check if it is an array first
-			int count = element.getChildFieldList().get(childField.fieldName)
+  private void constructJSONContentfromXML(Map parent, XMLElementContainer element) {
+    Map<String, List> jsonArrayTracker = new LinkedHashMap<String, List>()
+    // Process all the child fields of the XML element
+    element.getChildFields().each { childField ->
+      // Check if it is an array first
+      int count = element.getChildFieldList().get(childField.fieldName)
 
-			Object fieldContent = childField.fieldContent
-			if (fieldContent instanceof XMLElementContainer) {
-				// If it is a segment, create a JSON object for it first before
-				// adding into parent
-				JSONObject childjo = new JSONObject()
-				constructJSONContentfromXML(childjo, (XMLElementContainer) fieldContent)
-				putObjectIntoJSONObject(count, map, parent, childField.fieldName, childjo)
-			} else if (fieldContent instanceof String) {
-                // Check if any field conversion is required
-                if (this.fieldConversions.containsKey(childField.fieldName)) {
-					switch (this.fieldConversions.get(childField.fieldName)) {
-						case 'null':
-							putObjectIntoJSONObject(count, map, parent, childField.fieldName, null)
-							break
-						case 'number':
-							putObjectIntoJSONObject(count, map, parent, childField.fieldName, fieldContent as Double)
-							break
-						case 'boolean':
-							putObjectIntoJSONObject(count, map, parent, childField.fieldName, fieldContent.toBoolean())
-							break
-					}
-                } else
-					// If it is a string, directly add it to parent
-					putObjectIntoJSONObject(count, map, parent, childField.fieldName, fieldContent)
-			}
-			// If a JSONArray is created for this field and hasn't been added to
-			// the parent, then add the JSONArray to the parent
-			if (map.containsKey(childField.fieldName) && !parent.has(childField.fieldName)) {
-				parent.put(childField.fieldName, map.get(childField.fieldName))
-			}
-		}
-	}
+      Object fieldContent = childField.fieldContent
+      switch (fieldContent) {
+        case XMLElementContainer:
+          // If it is a segment, create a JSON object for it first before
+          // adding into parent
+          Map childObject = new LinkedHashMap()
+          constructJSONContentfromXML(childObject, (XMLElementContainer) fieldContent)
+          putIntoJSONObject(count, jsonArrayTracker, parent, childField.fieldName, childObject)
+          break
+        default:
+          // Check if any field conversion is required
+          if (this.fieldConversions.containsKey(childField.fieldName)) {
+            switch (this.fieldConversions.get(childField.fieldName)) {
+              case 'null':
+                putIntoJSONObject(count, jsonArrayTracker, parent, childField.fieldName, null)
+                break
+              case 'number':
+                putIntoJSONObject(count, jsonArrayTracker, parent, childField.fieldName, fieldContent as BigDecimal)
+                break
+              case 'boolean':
+                putIntoJSONObject(count, jsonArrayTracker, parent, childField.fieldName, fieldContent.toBoolean())
+                break
+            }
+          } else
+          // If it is a string, directly add it to parent
+            putIntoJSONObject(count, jsonArrayTracker, parent, childField.fieldName, fieldContent)
+          break
+      }
+      // If a JSONArray is created for this field and hasn't been added to
+      // the parent, then add the JSONArray to the parent
+      if (jsonArrayTracker.containsKey(childField.fieldName) && !parent.containsKey(childField.fieldName)) {
+        parent.put(childField.fieldName, jsonArrayTracker.get(childField.fieldName))
+      }
+    }
+  }
 
-	private void putObjectIntoJSONObject(int fieldCount, Map<String, JSONArray> jsonArrMap, JSONObject parent,
-			String fieldName, Object child) {
-		// If it is an array, put it into the corresponding JSON array in the
-		// map
-		if (fieldCount > 1 || this.forceArray || (this.arrayFields != null && this.arrayFields.contains(fieldName))) {
-			JSONArray ja = getJSONArray(jsonArrMap, fieldName)
-			ja.put(child)
-		} else {
-			// Otherwise directly put it into the parent
-			parent.put(fieldName, child)
-		}
-	}
+  private void putIntoJSONObject(int fieldCount, Map<String, List> jsonArrayTracker, Map parent,
+                                 String fieldName, Object child) {
+    // If it is an array, put it into the corresponding JSON array in the
+    // map
+    if (fieldCount > 1 || this.forceArray || (this.arrayFields != null && this.arrayFields.contains(fieldName))) {
+      List jsonArray = getJSONArray(jsonArrayTracker, fieldName)
+      jsonArray.add(child)
+    } else {
+      // Otherwise directly put it into the parent
+      parent.put(fieldName, child)
+    }
+  }
 
-	private String getJSONText(JSONObject jo, int indentFactor) {
-		return jo.toString(indentFactor)
-	}
+  private String getJSONText(Map jo, int indentFactor) {
+// TODO - Switch to JSON Generator in Groovy 2.5
+//    JsonGenerator generator = new JsonGenerator.Options()
+//            .excludeNulls()
+//            .disableUnicodeEscaping()
+//            .build()
+    if (indentFactor)
+      return JsonOutput.prettyPrint(JsonOutput.toJson(jo))
+    else
+      return JsonOutput.toJson(jo)
+  }
 
-	private JSONArray getJSONArray(Map<String, JSONArray> map, String arrayName) {
-		// Get the current JSONArray for this key or create a new JSONArray
-		if (map.containsKey(arrayName)) {
-			return map.get(arrayName)
-		} else {
-			JSONArray ja = new JSONArray()
-			map.put(arrayName, ja)
-			return ja
-		}
-	}
+  private List getJSONArray(Map<String, List> jsonArrayTracker, String arrayName) {
+    // Get the current JSONArray for this key or create a new JSONArray
+    if (jsonArrayTracker.containsKey(arrayName)) {
+      return jsonArrayTracker.get(arrayName)
+    } else {
+      def jsonArray = []
+      jsonArrayTracker.put(arrayName, jsonArray)
+      return jsonArray
+    }
+  }
 }
